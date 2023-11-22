1836c7412a02725a24f4b22b8fac48bdb43eef33
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2023-11-22 15:32:08 -0300

    sobreNos e Contato Routes

--- project/global.js
+++ project/global.js
@@ -1,8 +1,26 @@
 const body = $('body');
 const darkModeBtn = $('#dark-mode-btn');
+const contatoLinkBtn = $('#contato-link-btn');
+const sobreNosLinkBtn = $('#sobre-nos-link-btn');
 
 darkModeBtn.click(function (e) { 
     e.preventDefault();
     body.toggleClass('darkMode');
     darkModeBtn.text(body.hasClass('darkMode') ? 'Modo Claro' : 'Modo Escuro');
+});
+
+contatoLinkBtn.click(function (e) {
+    e.preventDefault();
+    
+    $('html, body').animate({
+        scrollTop: $('.support').offset().top
+    }, 1000);
+});
+
+sobreNosLinkBtn.click(function (e) {
+    e.preventDefault();
+    
+    $('html, body').animate({
+        scrollTop: $('#footer').offset().top
+    }, 1000);
 });
