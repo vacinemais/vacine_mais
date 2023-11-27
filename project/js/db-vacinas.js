d177e960f6acc97c1d9147274996d375eb91351c
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2023-11-27 20:10:08 -0300

    antesPatch

--- project/js/db-vacinas.js
+++ project/js/db-vacinas.js
@@ -1,138 +1,3 @@
-var vacinasdb = [
-  {
-    "doenca": "Sarampo",
-    "dose": "Dose 1",
-    "fabricante": "Fiocruz",
-    "data": "2009/10/20"
-  },
-  {
-    "doenca": "Hepatite A",
-    "dose": "Dose 1",
-    "fabricante": "Instituto Butantan",
-    "data": "2009/12/20"
-  },
-  {
-    "doenca": "Tétano",
-    "dose": "Dose 1",
-    "fabricante": "Instituto Butantan",
-    "data": "2010/01/05"
-  },
-  {
-    "doenca": "Hepatite A",
-    "dose": "Dose 2",
-    "fabricante": "Instituto Butantan",
-    "data": "2010/02/20"
-  },
-  {
-    "doenca": "Febre Amarela",
-    "dose": "Dose 1",
-    "fabricante": "Fiocruz",
-    "data": "2010/04/05"
-  },
-  {
-    "doenca": "Tríplice Viral",
-    "dose": "Dose 1",
-    "fabricante": "Fiocruz",
-    "data": "2011/10/17"
-  },
-  {
-    "doenca": "Hepatite B",
-    "dose": "Dose 1",
-    "fabricante": "Instituto Butantan",
-    "data": "2012/01/26"
-  },
-  {
-    "doenca": "Hepatite B",
-    "dose": "Dose 2",
-    "fabricante": "Instituto Butantan",
-    "data": "2012/02/26"
-  },
-  {
-    "doenca": "Hepatite B",
-    "dose": "Dose 3",
-    "fabricante": "Instituto Butantan",
-    "data": "2012/07/26"
-  },
-  {
-    "doenca": "Tétano",
-    "dose": "Dose 2",
-    "fabricante": "Instituto Butantan",
-    "data": "2020/02/07"
-  },
-  {
-    "doenca": "Meningite B",
-    "dose": "Dose 1",
-    "fabricante": "BioNTech",
-    "data": "2022/05/15"
-  },
-  {
-    "doenca": "Pneumonia",
-    "dose": "Dose 1",
-    "fabricante": "Moderna",
-    "data": "2022/09/25"
-  },
-  {
-    "doenca": "Covid-19",
-    "dose": "Dose 1",
-    "fabricante": "Pfizer",
-    "data": "2022/12/10"
-  },
-  {
-    "doenca": "Rubéola",
-    "dose": "Dose 1",
-    "fabricante": "Fiocruz",
-    "data": "2013/03/15"
-  },
-  {
-    "doenca": "Hepatite A",
-    "dose": "Dose 3",
-    "fabricante": "Instituto Butantan",
-    "data": "2013/07/18"
-  },
-  {
-    "doenca": "Caxumba",
-    "dose": "Dose 1",
-    "fabricante": "Fiocruz",
-    "data": "2013/11/22"
-  },
-  {
-    "doenca": "Difteria",
-    "dose": "Dose 1",
-    "fabricante": "Instituto Butantan",
-    "data": "2014/04/02"
-  },
-  {
-    "doenca": "Hepatite B",
-    "dose": "Dose 4",
-    "fabricante": "Instituto Butantan",
-    "data": "2014/08/10"
-  },
-  {
-    "doenca": "Tétano",
-    "dose": "Dose 3",
-    "fabricante": "Instituto Butantan",
-    "data": "2014/12/15"
-  },
-  {
-    "doenca": "Pneumonia",
-    "dose": "Dose 2",
-    "fabricante": "Moderna",
-    "data": "2015/03/21"
-  },
-  {
-    "doenca": "Tríplice Viral",
-    "dose": "Dose 2",
-    "fabricante": "Fiocruz",
-    "data": "2015/07/30"
-  },
-  {
-    "doenca": "Covid-19",
-    "dose": "Dose 2",
-    "fabricante": "Pfizer",
-    "data": "2023/02/05"
-  }
-];
-
 function atualizarLista(array) {
   const table = $("table tbody");
 
@@ -176,9 +41,9 @@ function atualizarTabela() {
     url: 'http://localhost:3000/vaccines',
     success: function (response) {
       let dbVacinas = [];
-      
+
       response.forEach((item, index) => {
-        const actionsCell = '<td><button class="btn btn-sm btn-outline-primary mr-2" data-bs-toggle="modal" data-bs-target="#editar-vacina-modal">Editar</button><button class="btn btn-sm btn-outline-danger">Excluir</button></td>';
+        const actionsCell = '<td><button class="btn btn-sm btn-outline-primary mr-2" data-bs-toggle="modal" data-bs-target="#editar-vacina-modal" onclick="editarVacina($(this))">Editar</button><button class="btn btn-sm btn-outline-danger" onClick="deletarVacina($(this))">Excluir</button></td>';
 
         const vacina = {
           "id": index + 1,
@@ -201,7 +66,7 @@ function atualizarTabela() {
           { data: 'dose' },
           { data: 'manufacturer' },
           { data: 'date' },
-          { data: 'actions'}
+          { data: 'actions' }
         ],
         "oLanguage": {
           "sSearch": ""
@@ -220,6 +85,26 @@ function atualizarTabela() {
   });
 }
 
+function deletarVacina(button) {
+  var row = button.closest('tr');
+  var elements = row.find('td');
+
+  let name = elements[1].innerText;
+  let dose = elements[2].innerText;
+  let manufacturer = elements[3].innerText;
+  let date = elements[4].innerText;
+
+  $.ajax({
+    type: 'DELETE',
+    url: `http://localhost:3000/vaccine/${name}/${dose}/${manufacturer}/${date}`,
+    success: function (response) {
+      location.reload();
+    },
+    error: function (error) {
+      console.error('Erro:', error);
+    }
+  });
+}
 
 $(document).ready(function () {
   atualizarTabela();
@@ -239,7 +124,8 @@ $(document).ready(function () {
       url: 'http://localhost:3000/vaccine',
       data: formData,
       success: function (response) {
-        console.log('Sucesso:', response);
+        console.log('Sucesso:', response); 
+        location.reload();
       },
       error: function (error) {
         console.error('Erro:', error);
