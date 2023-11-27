232722b1a0afe064a95615e2a4715c9625f400ce
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2023-11-27 17:08:18 -0300

    tabelaLocalStorage

--- project/js/db-vacinas.js
+++ project/js/db-vacinas.js
@@ -1,201 +1,228 @@
 var vacinasdb = [
-    {
-      "doenca": "Sarampo",
-      "dose": "Dose 1",
-      "fabricante": "Fiocruz",
-      "data": "2009/10/20"
-    },
-    {
-      "doenca": "Hepatite A",
-      "dose": "Dose 1",
-      "fabricante": "Instituto Butantan",
-      "data": "2009/12/20"
-    },
-    {
-      "doenca": "Tétano",
-      "dose": "Dose 1",
-      "fabricante": "Instituto Butantan",
-      "data": "2010/01/05"
-    },
-    {
-      "doenca": "Hepatite A",
-      "dose": "Dose 2",
-      "fabricante": "Instituto Butantan",
-      "data": "2010/02/20"
-    },
-    {
-      "doenca": "Febre Amarela",
-      "dose": "Dose 1",
-      "fabricante": "Fiocruz",
-      "data": "2010/04/05"
-    },
-    {
-      "doenca": "Tríplice Viral",
-      "dose": "Dose 1",
-      "fabricante": "Fiocruz",
-      "data": "2011/10/17"
-    },
-    {
-      "doenca": "Hepatite B",
-      "dose": "Dose 1",
-      "fabricante": "Instituto Butantan",
-      "data": "2012/01/26"
-    },
-    {
-      "doenca": "Hepatite B",
-      "dose": "Dose 2",
-      "fabricante": "Instituto Butantan",
-      "data": "2012/02/26"
-    },
-    {
-      "doenca": "Hepatite B",
-      "dose": "Dose 3",
-      "fabricante": "Instituto Butantan",
-      "data": "2012/07/26"
-    },
-    {
-      "doenca": "Tétano",
-      "dose": "Dose 2",
-      "fabricante": "Instituto Butantan",
-      "data": "2020/02/07"
-    },
-    {
-      "doenca": "Meningite B",
-      "dose": "Dose 1",
-      "fabricante": "BioNTech",
-      "data": "2022/05/15"
-    },
-    {
-      "doenca": "Pneumonia",
-      "dose": "Dose 1",
-      "fabricante": "Moderna",
-      "data": "2022/09/25"
-    },
-    {
-      "doenca": "Covid-19",
-      "dose": "Dose 1",
-      "fabricante": "Pfizer",
-      "data": "2022/12/10"
-    },
-    {
-        "doenca": "Rubéola",
-        "dose": "Dose 1",
-        "fabricante": "Fiocruz",
-        "data": "2013/03/15"
-    },
-    {
-        "doenca": "Hepatite A",
-        "dose": "Dose 3",
-        "fabricante": "Instituto Butantan",
-        "data": "2013/07/18"
-    },
-    {
-        "doenca": "Caxumba",
-        "dose": "Dose 1",
-        "fabricante": "Fiocruz",
-        "data": "2013/11/22"
-    },
-    {
-        "doenca": "Difteria",
-        "dose": "Dose 1",
-        "fabricante": "Instituto Butantan",
-        "data": "2014/04/02"
-    },
-    {
-        "doenca": "Hepatite B",
-        "dose": "Dose 4",
-        "fabricante": "Instituto Butantan",
-        "data": "2014/08/10"
-    },
-    {
-        "doenca": "Tétano",
-        "dose": "Dose 3",
-        "fabricante": "Instituto Butantan",
-        "data": "2014/12/15"
-    },
-    {
-        "doenca": "Pneumonia",
-        "dose": "Dose 2",
-        "fabricante": "Moderna",
-        "data": "2015/03/21"
-    },
-    {
-        "doenca": "Tríplice Viral",
-        "dose": "Dose 2",
-        "fabricante": "Fiocruz",
-        "data": "2015/07/30"
-    },
-    {
-        "doenca": "Covid-19",
-        "dose": "Dose 2",
-        "fabricante": "Pfizer",
-        "data": "2023/02/05"
-    }
+  {
+    "doenca": "Sarampo",
+    "dose": "Dose 1",
+    "fabricante": "Fiocruz",
+    "data": "2009/10/20"
+  },
+  {
+    "doenca": "Hepatite A",
+    "dose": "Dose 1",
+    "fabricante": "Instituto Butantan",
+    "data": "2009/12/20"
+  },
+  {
+    "doenca": "Tétano",
+    "dose": "Dose 1",
+    "fabricante": "Instituto Butantan",
+    "data": "2010/01/05"
+  },
+  {
+    "doenca": "Hepatite A",
+    "dose": "Dose 2",
+    "fabricante": "Instituto Butantan",
+    "data": "2010/02/20"
+  },
+  {
+    "doenca": "Febre Amarela",
+    "dose": "Dose 1",
+    "fabricante": "Fiocruz",
+    "data": "2010/04/05"
+  },
+  {
+    "doenca": "Tríplice Viral",
+    "dose": "Dose 1",
+    "fabricante": "Fiocruz",
+    "data": "2011/10/17"
+  },
+  {
+    "doenca": "Hepatite B",
+    "dose": "Dose 1",
+    "fabricante": "Instituto Butantan",
+    "data": "2012/01/26"
+  },
+  {
+    "doenca": "Hepatite B",
+    "dose": "Dose 2",
+    "fabricante": "Instituto Butantan",
+    "data": "2012/02/26"
+  },
+  {
+    "doenca": "Hepatite B",
+    "dose": "Dose 3",
+    "fabricante": "Instituto Butantan",
+    "data": "2012/07/26"
+  },
+  {
+    "doenca": "Tétano",
+    "dose": "Dose 2",
+    "fabricante": "Instituto Butantan",
+    "data": "2020/02/07"
+  },
+  {
+    "doenca": "Meningite B",
+    "dose": "Dose 1",
+    "fabricante": "BioNTech",
+    "data": "2022/05/15"
+  },
+  {
+    "doenca": "Pneumonia",
+    "dose": "Dose 1",
+    "fabricante": "Moderna",
+    "data": "2022/09/25"
+  },
+  {
+    "doenca": "Covid-19",
+    "dose": "Dose 1",
+    "fabricante": "Pfizer",
+    "data": "2022/12/10"
+  },
+  {
+    "doenca": "Rubéola",
+    "dose": "Dose 1",
+    "fabricante": "Fiocruz",
+    "data": "2013/03/15"
+  },
+  {
+    "doenca": "Hepatite A",
+    "dose": "Dose 3",
+    "fabricante": "Instituto Butantan",
+    "data": "2013/07/18"
+  },
+  {
+    "doenca": "Caxumba",
+    "dose": "Dose 1",
+    "fabricante": "Fiocruz",
+    "data": "2013/11/22"
+  },
+  {
+    "doenca": "Difteria",
+    "dose": "Dose 1",
+    "fabricante": "Instituto Butantan",
+    "data": "2014/04/02"
+  },
+  {
+    "doenca": "Hepatite B",
+    "dose": "Dose 4",
+    "fabricante": "Instituto Butantan",
+    "data": "2014/08/10"
+  },
+  {
+    "doenca": "Tétano",
+    "dose": "Dose 3",
+    "fabricante": "Instituto Butantan",
+    "data": "2014/12/15"
+  },
+  {
+    "doenca": "Pneumonia",
+    "dose": "Dose 2",
+    "fabricante": "Moderna",
+    "data": "2015/03/21"
+  },
+  {
+    "doenca": "Tríplice Viral",
+    "dose": "Dose 2",
+    "fabricante": "Fiocruz",
+    "data": "2015/07/30"
+  },
+  {
+    "doenca": "Covid-19",
+    "dose": "Dose 2",
+    "fabricante": "Pfizer",
+    "data": "2023/02/05"
+  }
 ];
-  
 
 function atualizarLista(array) {
-    const table = $("table tbody");
+  const table = $("table tbody");
 
-    array.forEach((item, index) => {
-        const row = $("<tr>");
+  array.forEach((item, index) => {
+    const row = $("<tr>");
 
-        const serialNumberCell = $("<td>").text(index + 1);
-        const diseaseCell = $("<td>").text(item.doenca);
-        const doseCell = $("<td>").text(item.dose);
-        const manufacturerCell = $("<td>").text(item.fabricante);
-        const dateCell = $("<td>").text(item.data);
+    const serialNumberCell = $("<td>").text(index + 1);
+    const diseaseCell = $("<td>").text(item.name);
+    const doseCell = $("<td>").text("Dose" + item.dose);
+    const manufacturerCell = $("<td>").text(item.manufacturer);
+    const dateCell = $("<td>").text(item.date);
 
-        const editButton = $("<button>")
-            .addClass("btn btn-sm btn-outline-primary mr-2")
-            .attr("data-bs-toggle", "modal")
-            .attr("data-bs-target", "#editar-vacina-modal")
-            .text("Editar");
+    const editButton = $("<button>")
+      .addClass("btn btn-sm btn-outline-primary mr-2")
+      .attr("data-bs-toggle", "modal")
+      .attr("data-bs-target", "#editar-vacina-modal")
+      .text("Editar");
 
-        const deleteButton = $("<button>")
-            .addClass("btn btn-sm btn-outline-danger")
-            .text("Excluir");
+    const deleteButton = $("<button>")
+      .addClass("btn btn-sm btn-outline-danger")
+      .text("Excluir");
 
-        const actionsCell = $("<td>").append(editButton, deleteButton);
+    const actionsCell = $("<td>").append(editButton, deleteButton);
 
-        
+    row.append(
+      serialNumberCell,
+      diseaseCell,
+      doseCell,
+      manufacturerCell,
+      dateCell,
+      actionsCell
+    );
 
-        row.append(
-            serialNumberCell,
-            diseaseCell,
-            doseCell,
-            manufacturerCell,
-            dateCell,
-            actionsCell
-        );
-
-        table.append(row);
-    });
+    table.append(row);
+  });
 }
 
-function configurarTabela() {
-
+function getVaccines() {
+  $.ajax({
+    type: 'GET',
+    url: 'http://localhost:3000/vaccines',
+    success: function (response) {
+      localStorage.setItem('vaccines', JSON.stringify(response));
+    }
+  });
 }
 
-$(document).ready(function() {
-    atualizarLista(vacinasdb);
+$(document).ready(function () {
+  getVaccines();
+  let dbVaccines = JSON.parse(localStorage.getItem('vaccines'));
 
-    const tabela = $('table');
+  atualizarLista(dbVaccines);
 
-    $('table').DataTable({
-        "oLanguage": {
-            "sSearch": ""
-        },
-        "lengthChange": false,
-        "pageLength": -1, 
-        "paging": false,
-        "info": false, 
-        "searching": true,
-        "scrollY": "350px",
-        "scrollCollapse": true,
+  const tabela = $('table');
+
+  $('table').DataTable({
+    "oLanguage": {
+      "sSearch": ""
+    },
+    "lengthChange": false,
+    "pageLength": -1,
+    "paging": false,
+    "info": false,
+    "searching": true,
+    "scrollY": "350px",
+    "scrollCollapse": true,
+  });
+
+  $('.dataTables_filter label input').addClass('form-control form-control-sm mt-3').attr('placeholder', 'Qual vacina você procura?');
+
+  $('#form-adicionar-vacina').submit(function (e) {
+    e.preventDefault();
+
+    var formData = {
+      name: $('#ipVacinaNome').val(),
+      dose: $('#ipVacinaDose').val(),
+      manufacturer: $('#ipVacinaFabricante').val(),
+      date: $('#ipVacinaData').val()
+    };
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:3000/vaccine',
+      data: formData,
+      success: function (response) {
+        console.log('Sucesso:', response);
+      },
+      error: function (error) {
+        console.error('Erro:', error);
+      }
     });
-
-    $('.dataTables_filter label input').addClass('form-control form-control-sm mt-3').attr('placeholder', 'Qual vacina você procura?'); 
-
-    
+  });
 }); 
